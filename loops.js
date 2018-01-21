4659a16c5ac2baca67a30d01f983a06b008cc136
Talei Sagawinit <taleipono@gmail.com>
2018-01-21 19:37:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,7 +25,7 @@ function doWhileLoop(array) {
   return Math.random() >= 0.5
 }
   do {
-    (var newArray = array.shift())
+    (array.shift())
   }
    while (array.length > 0 && maybeTrue())
    return array
